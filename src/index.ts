8fc11c113c145e76a5076bd16cd8399c21dba759
DaanV2 <daanverstraten@hotmail.com>
2024-07-20 15:28:11 +0200

    fixing up project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-
-
-
+export * as Enums from "./enums";
+export * as Blocks from "./blocks";
+export * as BlockProperties from "./block-properties";
+export * as Commands from "./commands";
+export * from "./util";
